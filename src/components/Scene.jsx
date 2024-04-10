a49d3d4353d8d9682d751db3e1db27caea3f9439
Declan <declan@nomlas.design>
2024-04-10 23:36:00 +1000

    Tidied up

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,28 +1,38 @@
 /* eslint-disable react/no-unknown-property */
 import { useRef } from 'react';
-import { useFrame, extend } from '@react-three/fiber';
+import { useFrame, extend, useThree } from '@react-three/fiber';
 import { shaderMaterial } from '@react-three/drei';
+import { useControls } from 'leva';
 
 // Shaders
 import vertex from '../shaders/vertex.js';
 import fragment from '../shaders/fragment.js';
 
 const Scene = () => {
-  const ref = useRef();
+  // Refs
+  const meshRef = useRef();
+  const matRef = useRef();
+
+  // Controls
+  const controls = useControls({
+    height: { value: 1, min: 0, max: 2, step: 0.01 },
+    width: { value: 1, min: 0, max: 2, step: 0.01 },
+  });
 
   // Shader Material
   const ShaderMaterial = shaderMaterial({ uTime: 0 }, vertex, fragment);
   extend({ ShaderMaterial });
 
-  // Animation
-  useFrame(({ clock }) => {
-    ref.current.uTime = clock.getElapsedTime();
+  // Frame loop
+  useFrame((state) => {
+    // Uniforms update
+    matRef.current.uniforms.uTime.value = state.clock.elapsedTime;
   });
 
   return (
-    <mesh>
-      <planeGeometry args={[1, 1]} />
-      <shaderMaterial ref={ref} />
+    <mesh ref={meshRef}>
+      <planeGeometry args={[controls.height, controls.width]} />
+      <shaderMaterial ref={matRef} />
     </mesh>
   );
 };
